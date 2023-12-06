9d2359bb68b9f41593b71a2adbc40ca6fe59eeda
William Morales <william.rolando88@hotmail.com>
2023-12-05 19:18:32 -0500

    Extract component

---
+++ src/components/DashboardLayout/index.tsx
@@ -0,0 +1,96 @@
+import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import { FC, ReactNode } from "react";
+import { DRAWER_WIDTH, LINKS, PLACEHOLDER_LINKS } from "../../app/layout";
+
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+export const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
+  return (
+    <ThemeRegistry>
+      <AppBar position="fixed" sx={{ zIndex: 2000 }}>
+        <Toolbar sx={{ backgroundColor: "background.paper" }}>
+          <DashboardIcon
+            sx={{ color: "#444", mr: 2, transform: "translateY(-2px)" }}
+          />
+
+          <Typography variant="h6" color="text.primary">
+            Next.js App Router
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        sx={{
+          width: DRAWER_WIDTH,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: DRAWER_WIDTH,
+            boxSizing: "border-box",
+            top: ["48px", "56px", "64px"],
+            height: "auto",
+            bottom: 0,
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Divider />
+
+        <List>
+          {LINKS.map(({ text, href, icon: Icon }) => (
+            <ListItem key={href} disablePadding>
+              <ListItemButton component={Link} href={href}>
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+        <Divider sx={{ mt: "auto" }} />
+
+        <List>
+          {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          bgcolor: "background.default",
+          ml: `${DRAWER_WIDTH}px`,
+          mt: ["48px", "56px", "64px"],
+          p: 3,
+        }}
+      >
+        {children}
+      </Box>
+    </ThemeRegistry>
+  );
+};
