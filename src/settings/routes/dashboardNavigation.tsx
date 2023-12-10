f8a1494ab78b1532db0d42956bd8d6913cc8f84b
William Morales <william.rolando88@hotmail.com>
2023-12-10 15:53:36 -0500

    Delete commented code

--- src/settings/routes/dashboardNavigation.tsx
+++ src/settings/routes/dashboardNavigation.tsx
@@ -12,27 +12,8 @@ const navConfig: NavData[] = [
         path: ROUTES.private.calculator.index,
         icon: ICONS.file,
       },
-      { title: "Starred", path: ROUTES.private.starred, icon: ICONS.blog },
-      { title: "Tasks", path: ROUTES.private.tasks, icon: ICONS.blog },
     ],
   },
-  // Nested category example:
-  //
-  // {
-  //   subheader: "management",
-  //   items: [
-  //     {
-  //       title: "user",
-  //       path: PATH_DASHBOARD.user.root,
-  //       icon: ICONS.user,
-  //       children: [
-  //         { title: "Four", path: PATH_DASHBOARD.user.four },
-  //         { title: "Five", path: PATH_DASHBOARD.user.five },
-  //         { title: "Six", path: PATH_DASHBOARD.user.six },
-  //       ],
-  //     },
-  //   ],
-  // },
 ];
 
 export default navConfig;
