fb7b977885b384ef742e2073902fe933b84dbd72
William Morales <william.rolando88@hotmail.com>
2023-12-09 22:38:29 -0500

    Add iconify

--- src/components/shared/Iconify.tsx
+++ src/components/shared/Iconify.tsx
@@ -1,24 +1,21 @@
-import { Icon, IconifyIcon } from '@iconify-icon/react';
-import { Box, BoxProps } from '@mui/material';
-import { FC } from 'react';
+import { Icon, IconifyIcon } from "@iconify-icon/react";
+import { Box, BoxProps } from "@mui/material";
+import { FC } from "react";
 
 export type IconifyProps = IconifyIcon | string;
 
 interface Props
-  extends Omit<
-    BoxProps,
-    'width' | 'height' | 'display' | 'alignItems' | 'justifyContent'
-  > {
+  extends Omit<BoxProps, "width" | "height" | "display" | "alignItems" | "justifyContent"> {
   icon: IconifyProps;
   size?: number;
 }
 
 const Iconify: FC<Props> = ({ icon, size = 20, sx, ...other }) => (
   <Box
-    display='flex'
-    alignItems='center'
-    justifyContent='center'
-    sx={{ width: 'fit-content', height: 'fit-content', ...sx }}
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    sx={{ width: "fit-content", height: "fit-content", ...sx }}
     {...other}
   >
     <Icon icon={icon} width={size} height={size} />
