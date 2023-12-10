bba19ca22fa7dcf10c44ae8fdca4a394c88e5a56
William Morales <william.rolando88@hotmail.com>
2023-12-09 23:00:38 -0500

    Add Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import ThemeRegistry from "@/settings/theme/wrappers/ThemeRegistry";
 import { ReactNode } from "react";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "simplebar-react/dist/simplebar.min.css";
 
 export const metadata = {
   title: "Public page",
--- src/components/shared/image/Image.tsx
+++ src/components/shared/image/Image.tsx
@@ -1,20 +1,18 @@
-import { Box } from '@mui/material';
-import { forwardRef } from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import getRatio from './getRatio';
-import { ImageProps } from './types';
+import { Box } from "@mui/material";
+import { forwardRef } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import getRatio from "./getRatio";
+import { ImageProps } from "./types";
 
 const Image = forwardRef<HTMLSpanElement, ImageProps>(
-  ({ ratio, disabledEffect = false, effect = 'blur', sx, ...other }, ref) => {
+  ({ ratio, disabledEffect = false, effect = "blur", sx, ...other }, ref) => {
     const content = (
       <Box
         component={LazyLoadImage}
-        wrapperClassName='wrapper'
+        wrapperClassName="wrapper"
         effect={disabledEffect ? undefined : effect}
-        placeholderSrc={
-          disabledEffect ? '/assets/transparent.png' : '/assets/placeholder.svg'
-        }
-        sx={{ width: 1, height: 1, objectFit: 'cover' }}
+        placeholderSrc={disabledEffect ? "/assets/transparent.png" : "/assets/placeholder.svg"}
+        sx={{ width: 1, height: 1, objectFit: "cover" }}
         {...other}
       />
     );
@@ -23,21 +21,21 @@ const Image = forwardRef<HTMLSpanElement, ImageProps>(
       return (
         <Box
           ref={ref}
-          component='span'
+          component="span"
           sx={{
             width: 1,
             lineHeight: 1,
-            display: 'block',
-            overflow: 'hidden',
-            position: 'relative',
+            display: "block",
+            overflow: "hidden",
+            position: "relative",
             pt: getRatio(ratio),
-            '& .wrapper': {
+            "& .wrapper": {
               top: 0,
               left: 0,
               width: 1,
               height: 1,
-              position: 'absolute',
-              backgroundSize: 'cover !important',
+              position: "absolute",
+              backgroundSize: "cover !important",
             },
             ...sx,
           }}
@@ -50,16 +48,16 @@ const Image = forwardRef<HTMLSpanElement, ImageProps>(
     return (
       <Box
         ref={ref}
-        component='span'
+        component="span"
         sx={{
           lineHeight: 1,
-          display: 'block',
-          overflow: 'hidden',
-          position: 'relative',
-          '& .wrapper': {
+          display: "block",
+          overflow: "hidden",
+          position: "relative",
+          "& .wrapper": {
             width: 1,
             height: 1,
-            backgroundSize: 'cover !important',
+            backgroundSize: "cover !important",
           },
           ...sx,
         }}
