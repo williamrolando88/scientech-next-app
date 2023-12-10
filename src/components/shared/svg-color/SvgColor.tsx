23f3bda70de1a9598bde989e7bab2282d876e8f1
William Morales <william.rolando88@hotmail.com>
2023-12-10 07:52:02 -0500

    Add svg color component

---
+++ src/components/shared/svg-color/SvgColor.tsx
@@ -0,0 +1,26 @@
+import { Box, BoxProps } from "@mui/material";
+import { forwardRef } from "react";
+
+export interface SvgColorProps extends BoxProps {
+  src: string;
+}
+
+const SvgColor = forwardRef<HTMLSpanElement, SvgColorProps>(({ src, sx, ...other }, ref) => (
+  <Box
+    component="span"
+    className="svg-color"
+    ref={ref}
+    sx={{
+      width: 24,
+      height: 24,
+      display: "inline-block",
+      bgcolor: "currentColor",
+      mask: `url(${src}) no-repeat center / contain`,
+      WebkitMask: `url(${src}) no-repeat center / contain`,
+      ...sx,
+    }}
+    {...other}
+  />
+));
+
+export default SvgColor;
