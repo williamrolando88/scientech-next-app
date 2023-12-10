039b94c430de48f5194fcb979129a0bb42291326
William Morales <william.rolando88@hotmail.com>
2023-12-10 07:15:06 -0500

    Add search not found component

--- src/settings/theme/wrappers/ThemeRegistry.tsx
+++ src/settings/theme/wrappers/ThemeRegistry.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { MotionLazyContainer } from "@/components/shared/animate";
+import ProgressBar from "@/components/shared/progress-bar";
 import SnackbarProvider from "@/components/shared/snackbar/SnackbarProvider";
 import ThemeProvider from "@/settings/theme";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -16,7 +17,10 @@ export default function ThemeRegistry({ children }: { children: React.ReactNode 
           <ThemeProvider>
             <CssBaseline />
             <ThemeSettings>
-              <SnackbarProvider>{children}</SnackbarProvider>
+              <SnackbarProvider>
+                <ProgressBar />
+                {children}
+              </SnackbarProvider>
             </ThemeSettings>
           </ThemeProvider>
         </NextAppDirEmotionCacheProvider>
