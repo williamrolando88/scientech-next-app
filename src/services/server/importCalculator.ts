db50d6fab4199e735575c7e9671603a6b2932d55
William Morales <william.rolando88@hotmail.com>
2023-12-17 18:34:23 -0500

    Add Import calculator server service

--- src/services/server/importCalculator.ts
+++ src/services/server/importCalculator.ts
@@ -1,4 +1,22 @@
-class ImportCalculatorServer {}
+import { FirestoreCollections } from "@/settings/firebase/firebaseAdmin.config";
+import { ImportCalculator } from "@/types/importCalculator";
+
+class ImportCalculatorServer {
+  collection = FirestoreCollections.importCalculations;
+
+  create = async (data: ImportCalculator) => {
+    data.metadata.createdAt = new Date().getTime();
+    data.metadata.updatedAt = new Date().getTime();
+    const docRef = await this.collection.add(data);
+    return docRef.id;
+  };
+
+  update = async (id: string, data: ImportCalculator) => {
+    data.metadata.updatedAt = new Date().getTime();
+    const response = await this.collection.doc(id).update(data);
+    return response.writeTime;
+  };
+}
 
 const ImportCalculatorStorage = new ImportCalculatorServer();
 
